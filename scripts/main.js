60b081d85556927f7340675368ae02ae52517767
Gustaf Petersen <gustaf.petersen@gmail.com>
2022-01-26 08:26:52 +0100

    updated changes for all documents.

--- scripts/main.js
+++ scripts/main.js
@@ -9,22 +9,37 @@ if (loadedVar === 1){
 
 let gridContainer = document.body.querySelector(".sketch-container"); 
 
-let etchSquares = 256;
-for (let i = 1; i <= etchSquares; i++){
+let etchSquares = 20;
+for (let i = 1; i <= (etchSquares*etchSquares); i++){
     var squares = document.createElement('div');
     squares.className = "etch-square";
     squares.id = "square-" + i;
+    squares.style.width = etchSquares + "px";
+    squares.style.height = etchSquares + "px";
+    gridContainer.style.gridTemplateColumns = `repeat(${etchSquares}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${etchSquares}, 1fr)`;
     gridContainer.appendChild(squares);
-    console.log(i + " div(s) has been created");
+    }
+
+const color
+const colorMod = edocument.body.querySelector(".color-mode");
+const blackMode = document.body.querySelector(".black-mode");
+const randomMode = document.body.querySelector(".random-mode");
+const eraserButton = document.body.querySelector(".eraser-button");
+const clearButton = document.body.querySelector(".clear-button");
+const rangeSlider = document.body.querySelector(".range-slider");
+
+const DEFAULT_COLOR = "#363636";
+const CURRENT_MODE = "color";
+
+const currentColor = DEFAULT_COLOR;
+const currentMode =  CURRENT_MODE;
+
+function setColor(newColor){
+    currentColor = newColor;
+}
+function setMode(newMode){
+    currentMode = newMode;
 }
 
-let numberRowColumn = Math.sqrt(etchSquares);
-
-console.log(numberRowColumn)
-
-const createGrid = () => {
-    gridContainer.style.gridTemplateRows = `repeat(${numberRowColumn}, 1 fr)`
-    // gridContainer.style.gridTemplateRows = `repeat(${numberRowColumn}, 1 fr)`
-}
-
-createGrid()
+colorButton.onchange = (e) => setColor(e.target.value)
