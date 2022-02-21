509ad54bb00dfec4710effba9fe3d07fd438af63
Gustaf Petersen <gustaf.petersen@gmail.com>
2022-02-21 20:55:42 +0100

    updated script functionality, html and css-styling

--- scripts/main.js
+++ scripts/main.js
@@ -1,45 +1,77 @@
 let loadedVar = 1;
-scriptLoaded = "The script is loaded!"
-scriptNotLoaded = "The script is loaded but the loadedVar isn't stored succesfully!"
-if (loadedVar === 1){
-    console.log(scriptLoaded)
+let scriptLoaded = "The script is loaded!";
+let scriptNotLoaded =
+  "The script is loaded but the loadedVar isn't stored succesfully!";
+if (loadedVar === 1) {
+  console.log(scriptLoaded);
 } else {
-    console.log(scriptNotLoaded)
+  console.log(scriptNotLoaded);
 }
 
-let gridContainer = document.body.querySelector(".sketch-container"); 
+let gridContainer = document.body.querySelector(".sketch-container");
+let squareRange = document.body.querySelector(".range-slider");
+let etchSquares = squareRange.value;
+const clearButton = document.body.querySelector(".clear-button");
 
-let etchSquares = 20;
-for (let i = 1; i <= (etchSquares*etchSquares); i++){
-    var squares = document.createElement('div');
+// squareRange.addEventListener("input", function (e) {
+//   console.log("detecting input change");
+//   //window.location.reload()
+//   etchSquares = squareRange.value;
+//   updateSquareStyles();
+// });
+
+squareRange.addEventListener("change", function (e) {
+  console.log("detecting input change");
+  //window.location.reload()
+  etchSquares = squareRange.value;
+  updateSquareStyles();
+});
+
+clearButton.addEventListener("click", function (e) {
+  window.location.reload();
+})
+
+const updateSquareStyles = () => {
+  console.log("creating new grid with size", etchSquares);
+  gridContainer.innerHTML = "";
+
+  for (let i = 1; i <= etchSquares * etchSquares; i++) {
+    var squares = document.createElement("div");
     squares.className = "etch-square";
     squares.id = "square-" + i;
-    squares.style.width = etchSquares + "px";
-    squares.style.height = etchSquares + "px";
+    squares.style.width = 600 / etchSquares + "px";
+    squares.style.height = 600 / etchSquares + "px";
     gridContainer.style.gridTemplateColumns = `repeat(${etchSquares}, 1fr)`;
     gridContainer.style.gridTemplateRows = `repeat(${etchSquares}, 1fr)`;
     gridContainer.appendChild(squares);
-    }
+  }
 
-const color
-const colorMod = edocument.body.querySelector(".color-mode");
-const blackMode = document.body.querySelector(".black-mode");
-const randomMode = document.body.querySelector(".random-mode");
-const eraserButton = document.body.querySelector(".eraser-button");
-const clearButton = document.body.querySelector(".clear-button");
-const rangeSlider = document.body.querySelector(".range-slider");
+  const hoverEffect = document.body.querySelectorAll(".etch-square");
+  console.log(hoverEffect);
 
-const DEFAULT_COLOR = "#363636";
-const CURRENT_MODE = "color";
+  for (var i = 0; i < hoverEffect.length; i++) {
+    hoverEffect[i].addEventListener("mouseover", function (e) {
+      e.target.style.backgroundColor = colorPicker.value;
+    });
+  }
+};
 
-const currentColor = DEFAULT_COLOR;
-const currentMode =  CURRENT_MODE;
 
-function setColor(newColor){
-    currentColor = newColor;
-}
-function setMode(newMode){
-    currentMode = newMode;
-}
+// const clickEffect = document.body.querySelectorAll(".etch-square");
+// console.log(clickEffect);
 
-colorButton.onchange = (e) => setColor(e.target.value)
+// for (var i = 0; i < clickEffect.length; i++) {
+//   hoverEffect[i].addEventListener("click", function (e) {
+//     e.target.style.backgroundColor = colorPicker.value;
+//   });
+// }
+// };
+
+updateSquareStyles();
+
+let colorPicker = document.body.querySelector(".color-picker");
+
+colorPicker.addEventListener("input", function (e) {
+  let newColor = colorPicker.value;
+  console.log("color has been changed!", newColor);
+});
